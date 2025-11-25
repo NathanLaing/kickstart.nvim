285fe1ec1c0f373c386b0e1af582f316731800a5
Nathan Laing <nathan@phntms.com>
2025-11-26 11:28:43 +1300

    update config

--- init.lua
+++ init.lua
@@ -91,7 +91,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
+vim.opt.guifont = 'JetBrainsMono Nerd Font:h14'
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -172,6 +173,7 @@ vim.o.confirm = true
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('i', 'jk', '<Esc>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
@@ -684,6 +686,15 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        angularls = {},
+        eslint = {},
+        html = {},
+        jsonls = {},
+        pylsp = {},
+        pyright = {},
+        stylelint_lsp = {},
+        ts_ls = {},
+
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -772,7 +783,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
@@ -876,27 +888,57 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+  -- New Webstorm color scheme
+  {
+    'nickkadutskyi/jb.nvim',
+    lazy = false, -- Colorschemes should not be lazy loaded
+    priority = 1000, -- Set a high priority to load it before other UI plugins
+    opts = {
+      transparent = false,
+    },
+    config = function(_, opts)
+      require('jb').setup(opts)
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- This command applies the colorscheme immediately after the plugin loads
+      vim.cmd 'colorscheme jb'
+
+      -- If you want a transparent background, you can use:
+      -- require("jb").setup({ transparent = true })
+      -- vim.cmd("colorscheme jb")
     end,
   },
+
+  -- Old school Webstorm color scheme
+  --  {
+  --    'xiantang/darcula-dark.nvim',
+  --    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  --    priority = 1000,
+  --    config = function()
+  --      vim.cmd 'colorscheme darcula-dark'
+  --    end,
+  --  },
+
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('tokyonight').setup {
+  --       styles = {
+  --         comments = { italic = false }, -- Disable italics in comments
+  --       },
+  --     }
+
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -944,7 +986,38 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'angular',
+        'bash',
+        'c',
+        'css',
+        'diff',
+        'editorconfig',
+        'git_config',
+        'git_rebase',
+        'gitattributes',
+        'gitcommit',
+        'gitignore',
+        'http',
+        'javascript',
+        'jq',
+        'jsdoc',
+        'json',
+        'json5',
+        'jsonc',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'regex',
+        'scss',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'yaml',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -977,14 +1050,14 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,23 +1,104 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
+  branch = 'v3.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    {
+      '\\',
+      function()
+        -- If we are in Neo-tree, close it.
+        if vim.bo.filetype == 'neo-tree' then
+          vim.cmd.Neotree 'close'
+        else
+          -- Otherwise, open it and reveal the current file
+          vim.cmd.Neotree 'reveal'
+        end
+      end,
+      desc = 'NeoTree Reveal (Smart)',
+      silent = true,
+    },
   },
   opts = {
+    -- GLOBAL SETTINGS
+    enable_git_status = true,
+    enable_diagnostics = true,
+
+    -- VISUALS
+    default_component_configs = {
+      indent = {
+        with_markers = true,
+        indent_marker = '│',
+        last_indent_marker = '└',
+        indent_size = 2,
+        padding = 1,
+        with_expanders = true,
+        expander_collapsed = '▶',
+        expander_expanded = '▼',
+      },
+      icon = {
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = '󰜌',
+      },
+      name = {
+        use_git_status_colors = true,
+      },
+      git_status = {
+        symbols = {
+          added = '',
+          modified = '',
+          deleted = '✖',
+          renamed = '󰁕',
+          untracked = '',
+          ignored = '',
+          unstaged = '󰄱',
+          staged = '',
+          conflict = '',
+        },
+      },
+      diagnostics = {
+        symbols = {
+          hint = '',
+          info = '',
+          warn = '',
+          error = '',
+        },
+        highlights = {
+          hint = 'DiagnosticSignHint',
+          info = 'DiagnosticSignInfo',
+          warn = 'DiagnosticSignWarn',
+          error = 'DiagnosticSignError',
+        },
+      },
+    },
+
+    -- GLOBAL WINDOW CONFIG
+    window = {
+      position = 'right',
+      width = 35,
+      mappings = {
+        ['<space>'] = 'none',
+      },
+    },
+
+    -- FILESYSTEM SPECIFIC SETTINGS
     filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+      bind_to_cwd = false,
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          '.git',
         },
       },
     },
