6bfe5a24b5a6f7f19c3df720ee60629f8d2564f9
Nathan Laing <nathan@phntms.com>
2025-12-08 11:56:53 +1300

    Add autopairs plugin

--- init.lua
+++ init.lua
@@ -1125,9 +1125,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommended keymaps
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommended keymaps
 
   { import = 'custom.plugins' },
   --
